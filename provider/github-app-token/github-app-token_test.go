e2b443c655d78d0a46f7a15275b24584029eb0b8
actionchain <actionchain@chainguard.dev>
2026-06-20 09:32:16 +0000

    Update shogo82148/actions-github-app-token@v1.3.1

---
+++ provider/github-app-token/github-app-token_test.go
@@ -0,0 +1,235 @@
+package githubapptoken
+
+import (
+	"context"
+	"encoding/base64"
+	"errors"
+	"testing"
+
+	"github.com/shogo82148/actions-github-app-token/provider/github-app-token/github"
+	"github.com/shogo82148/goat/jwt"
+)
+
+type githubClientMock struct {
+	GetAppFunc               func(ctx context.Context) (*github.GetAppResponse, error)
+	GetReposInstallationFunc func(ctx context.Context, owner, repo string) (*github.GetReposInstallationResponse, error)
+	GetRepoFunc              func(ctx context.Context, token, owner, repo string) (*github.GetRepoResponse, error)
+	GetReposInfoFunc         func(ctx context.Context, token, nodeID string) (*github.GetReposInfoResponse, error)
+	GetReposContentFunc      func(ctx context.Context, token, owner, repo, path string) (*github.GetReposContentResponse, error)
+	CreateAppAccessTokenFunc func(ctx context.Context, installationID uint64, permissions *github.CreateAppAccessTokenRequest) (*github.CreateAppAccessTokenResponse, error)
+	ValidateAPIURLFunc       func(url string) error
+	ParseIDTokenFunc         func(ctx context.Context, idToken string) (*github.ActionsIDToken, error)
+	RevokeAppAccessTokenFunc func(ctx context.Context, token string) error
+}
+
+func (c *githubClientMock) GetApp(ctx context.Context) (*github.GetAppResponse, error) {
+	return c.GetAppFunc(ctx)
+}
+
+func (c *githubClientMock) GetReposInstallation(ctx context.Context, owner, repo string) (*github.GetReposInstallationResponse, error) {
+	return c.GetReposInstallationFunc(ctx, owner, repo)
+}
+
+func (c *githubClientMock) GetRepo(ctx context.Context, token, owner, repo string) (*github.GetRepoResponse, error) {
+	return c.GetRepoFunc(ctx, token, owner, repo)
+}
+
+func (c *githubClientMock) GetReposInfo(ctx context.Context, token, nodeID string) (*github.GetReposInfoResponse, error) {
+	return c.GetReposInfoFunc(ctx, token, nodeID)
+}
+
+func (c *githubClientMock) GetReposContent(ctx context.Context, token, owner, repo, path string) (*github.GetReposContentResponse, error) {
+	return c.GetReposContentFunc(ctx, token, owner, repo, path)
+}
+
+func (c *githubClientMock) CreateAppAccessToken(ctx context.Context, installationID uint64, permissions *github.CreateAppAccessTokenRequest) (*github.CreateAppAccessTokenResponse, error) {
+	return c.CreateAppAccessTokenFunc(ctx, installationID, permissions)
+}
+
+func (c *githubClientMock) ValidateAPIURL(url string) error {
+	return c.ValidateAPIURLFunc(url)
+}
+
+func (c *githubClientMock) ParseIDToken(ctx context.Context, idToken string) (*github.ActionsIDToken, error) {
+	return c.ParseIDTokenFunc(ctx, idToken)
+}
+
+func (c *githubClientMock) RevokeAppAccessToken(ctx context.Context, token string) error {
+	return c.RevokeAppAccessTokenFunc(ctx, token)
+}
+
+func TestHandle_Dummy(t *testing.T) {
+	h := NewDummyHandler()
+	_, err := h.handle(context.Background(), "dummy-token", &requestBody{
+		Repositories: []string{"R_123456"},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestHandle(t *testing.T) {
+	revoked := false
+	h := &Handler{
+		github: &githubClientMock{
+			ValidateAPIURLFunc: func(url string) error {
+				return nil
+			},
+			ParseIDTokenFunc: func(ctx context.Context, idToken string) (*github.ActionsIDToken, error) {
+				return &github.ActionsIDToken{
+					Claims: &jwt.Claims{
+						Audience: []string{"https://github-app.shogo82148.com/1234567890"},
+					},
+					Repository:   "shogo82148/actions-github-app-token",
+					RepositoryID: "398574950",
+				}, nil
+			},
+			GetRepoFunc: func(ctx context.Context, token, owner, repo string) (*github.GetRepoResponse, error) {
+				if token != "ghs_dummyGitHubToken" {
+					t.Errorf("unexpected token: got %q, want %q", token, "ghs_dummyGitHubToken")
+				}
+				return &github.GetRepoResponse{
+					ID:     398574950,
+					NodeID: "R_kgDOF8HFZg",
+				}, nil
+			},
+			GetReposInfoFunc: func(ctx context.Context, token, nodeID string) (*github.GetReposInfoResponse, error) {
+				if token != "ghs_dummyGitHubToken" {
+					t.Errorf("unexpected token: got %q, want %q", token, "ghs_dummyGitHubToken")
+				}
+				return &github.GetReposInfoResponse{
+					ID:    398574950,
+					Owner: "shogo82148",
+					Name:  "actions-github-app-token",
+				}, nil
+			},
+			GetReposContentFunc: func(ctx context.Context, token, owner, repo, path string) (*github.GetReposContentResponse, error) {
+				if token != "ghs_dummyGitHubToken" {
+					t.Errorf("unexpected token: got %q, want %q", token, "ghs_dummyGitHubToken")
+				}
+				content := "repositories:\n  - R_kgDOF8HFZg\n"
+				return &github.GetReposContentResponse{
+					Type:     "file",
+					Encoding: "base64",
+					Content:  base64.StdEncoding.EncodeToString([]byte(content)),
+				}, nil
+			},
+			GetReposInstallationFunc: func(ctx context.Context, owner, repo string) (*github.GetReposInstallationResponse, error) {
+				return &github.GetReposInstallationResponse{
+					ID: 641323,
+				}, nil
+			},
+			CreateAppAccessTokenFunc: func(ctx context.Context, installationID uint64, permissions *github.CreateAppAccessTokenRequest) (*github.CreateAppAccessTokenResponse, error) {
+				return &github.CreateAppAccessTokenResponse{
+					Token: "ghs_dummyGitHubToken",
+				}, nil
+			},
+			RevokeAppAccessTokenFunc: func(ctx context.Context, token string) error {
+				if token != "ghs_dummyGitHubToken" {
+					t.Errorf("unexpected token: got %q, want %q", token, "ghs_dummyGitHubToken")
+				}
+				revoked = true
+				return nil
+			},
+		},
+		appID: 1234567890,
+	}
+	resp, err := h.handle(context.Background(), "dummy-token", &requestBody{
+		Repositories: []string{
+			"R_kgDOIeornQ", "R_kgDOIevBqQ",
+		},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.GitHubToken != "ghs_dummyGitHubToken" {
+		t.Errorf("unexpected token: got %q, want %q", resp.GitHubToken, "ghs_dummyGitHubToken")
+	}
+	if !revoked {
+		t.Error("want revoked, but not")
+	}
+}
+
+func TestHandle_NoPermission(t *testing.T) {
+	revoked := false
+	h := &Handler{
+		github: &githubClientMock{
+			ValidateAPIURLFunc: func(url string) error {
+				return nil
+			},
+			ParseIDTokenFunc: func(ctx context.Context, idToken string) (*github.ActionsIDToken, error) {
+				return &github.ActionsIDToken{
+					Claims: &jwt.Claims{
+						Audience: []string{"https://github-app.shogo82148.com/1234567890"},
+					},
+					Repository:   "shogo82148/actions-github-app-token",
+					RepositoryID: "398574950",
+				}, nil
+			},
+			GetRepoFunc: func(ctx context.Context, token, owner, repo string) (*github.GetRepoResponse, error) {
+				if token != "ghs_dummyGitHubToken" {
+					t.Errorf("unexpected token: got %q, want %q", token, "ghs_dummyGitHubToken")
+				}
+				return &github.GetRepoResponse{
+					ID:     398574950,
+					NodeID: "R_kgDOF8HFZg",
+				}, nil
+			},
+			GetReposInfoFunc: func(ctx context.Context, token, nodeID string) (*github.GetReposInfoResponse, error) {
+				if token != "ghs_dummyGitHubToken" {
+					t.Errorf("unexpected token: got %q, want %q", token, "ghs_dummyGitHubToken")
+				}
+				return &github.GetReposInfoResponse{
+					ID:    398574950,
+					Owner: "shogo82148",
+					Name:  "actions-github-app-token",
+				}, nil
+			},
+			GetReposContentFunc: func(ctx context.Context, token, owner, repo, path string) (*github.GetReposContentResponse, error) {
+				if token != "ghs_dummyGitHubToken" {
+					t.Errorf("unexpected token: got %q, want %q", token, "ghs_dummyGitHubToken")
+				}
+				content := "repositories: []\n"
+				return &github.GetReposContentResponse{
+					Type:     "file",
+					Encoding: "base64",
+					Content:  base64.StdEncoding.EncodeToString([]byte(content)),
+				}, nil
+			},
+			GetReposInstallationFunc: func(ctx context.Context, owner, repo string) (*github.GetReposInstallationResponse, error) {
+				return &github.GetReposInstallationResponse{
+					ID: 641323,
+				}, nil
+			},
+			CreateAppAccessTokenFunc: func(ctx context.Context, installationID uint64, permissions *github.CreateAppAccessTokenRequest) (*github.CreateAppAccessTokenResponse, error) {
+				return &github.CreateAppAccessTokenResponse{
+					Token: "ghs_dummyGitHubToken",
+				}, nil
+			},
+			RevokeAppAccessTokenFunc: func(ctx context.Context, token string) error {
+				if token != "ghs_dummyGitHubToken" {
+					t.Errorf("unexpected token: got %q, want %q", token, "ghs_dummyGitHubToken")
+				}
+				revoked = true
+				return nil
+			},
+		},
+		appID: 1234567890,
+	}
+	_, err := h.handle(context.Background(), "dummy-token", &requestBody{
+		Repositories: []string{
+			"R_kgDOIeornQ", "R_kgDOIevBqQ",
+		},
+	})
+	if err == nil {
+		t.Fatal("want some error, but not")
+	}
+	if !revoked {
+		t.Error("want revoked, but not")
+	}
+
+	var forbidden *forbiddenError
+	if !errors.As(err, &forbidden) {
+		t.Fatalf("want *forbiddenError, but got %T", err)
+	}
+}
---
+++ provider/github-app-token/github/get_repos_installation.go
@@ -0,0 +1,53 @@
+package github
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/url"
+)
+
+type GetReposInstallationResponse struct {
+	ID uint64 `json:"id"`
+
+	// omit other fields, we don't use them.
+}
+
+// GetReposInstallation gets a repository installation for the authenticated app
+// https://docs.github.com/en/rest/reference/apps#get-a-repository-installation-for-the-authenticated-app
+func (c *Client) GetReposInstallation(ctx context.Context, owner, repo string) (*GetReposInstallationResponse, error) {
+	token, err := c.generateJWT(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// build the request
+	u := c.baseURL.JoinPath("repos", url.PathEscape(owner), url.PathEscape(repo), "installation")
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Accept", "application/vnd.github+json")
+	req.Header.Set("User-Agent", githubUserAgent)
+	req.Header.Set("Authorization", "Bearer "+token)
+	req.Header.Set("X-GitHub-Api-Version", githubAPIVersion)
+	req.Header.Set("X-Github-Next-Global-ID", "1")
+
+	// send the request
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// parse the response
+	if resp.StatusCode != http.StatusOK {
+		return nil, newErrUnexpectedStatusCode(resp)
+	}
+
+	var ret *GetReposInstallationResponse
+	if err := json.NewDecoder(resp.Body).Decode(&ret); err != nil {
+		return nil, err
+	}
+	return ret, nil
+}
